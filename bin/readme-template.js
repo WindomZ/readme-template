704e2946a30c1042656b0ee7da9aff9438ba2317
WindomZ <git.windomz@gmail.com>
2017-04-18 08:18:08 +0800

    update project

--- bin/readme-template.js
+++ bin/readme-template.js
@@ -1,3 +1,40 @@
+#!/usr/bin/env node
 /**
- * Created by Windom on 2017/4/18.
+ * Created by WindomZ on 2017/4/18.
  */
+'use strict'
+
+const process = require('process')
+
+const program = require('commander')
+
+const pkg = require('../package.json')
+
+let noArgs = true
+
+program
+  .version(pkg.version)
+  .usage('[options] <template>')
+  .description('Generate some common README.md templates for GitHub.')
+  .option('--debug', 'debug mode, such as print error tracks', null, null)
+  .action((template, options) => {
+    noArgs = false
+
+    console.log('template: %s', template)
+  })
+
+program
+  .command('list')
+  .alias('ls')
+  .description('List the optional README.md templates to generate them.')
+  .action((options) => {
+    noArgs = false
+
+    console.log('list')
+  })
+
+program.parse(process.argv)
+
+if (noArgs) {
+  program.outputHelp()
+}
